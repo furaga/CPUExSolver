c5683c41bd82457aff04311092215f1caa58c79e
furaga <furaga@furaga-laptop.(none)>
2012-03-13 19:56:27 +0900

    ライセンスなど

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -7,14 +7,6 @@ aboutdialog::aboutdialog(QWidget *parent) :
     ui(new Ui::aboutdialog)
 {
     ui->setupUi(this);
-
-    bool res = pixmap.load("./resources/ika.jpg");
-    pixmap = pixmap.scaled(
-                QSize(
-                    ui->graphicsView->width() - 5,
-                    ui->graphicsView->height() - 5));
-    scene.addPixmap(pixmap);
-    ui->graphicsView->setScene(&scene);
 }
 
 aboutdialog::~aboutdialog()
